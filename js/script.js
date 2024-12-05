ca55c587f5d3e87b5b0c01bd7c86ada737435317
L3on4rd0wen <l3on4rd30@gmail.com>
2024-12-05 08:05:30 +0700

    added gender, number only and code

--- js/script.js
+++ js/script.js
@@ -3,44 +3,43 @@ let gender = document.getElementById("gender");
 let weight = document.getElementById("displayW");
 let height = document.getElementById("displayH");
 let result = document.getElementById("result");
-let count = document.getElementById("count");
-let reset = document.getElementById("reset");
 
-// Nilai Tinggi dan Berat || Height and weight values
-let w = weight.value;
-let h = height.value;
+// memilih kelamin  || picks gender
+function rotate() {
+    if (gender.textContent === "Pria") {
+       gender.textContent = "Wanita";
+    } else {
+       gender.textContent = "Pria";
+    }
+}
+
 
 // Menghitung bmi || Calculates bmi
 function calculate() {
+    // Nilai Tinggi dan Berat || Height and weight values
+    const w = parseFloat(weight.value);
+    const h = parseFloat(height.value) / 100;
 
-    let bmi = w / ((h / 100) ** 2);
+    if (isNaN(w) || isNaN(h) || w <= 0 || h <= 0) {
+        alert("Tolong masukkan nilai berat dan tinggi badan yang valid dan positif. \n(Please enter valid positive values for weight and height.)");
+    } else {
+        const bmi = w / (h * h);
 
-    //Memilih kategori dan mencari error || Picks a category and finds errors
-    if (bmi < 18.5) {
-        result.textContent = "Kekurangan Berat Badan (Underweight)";
-    } else if (bmi < 24.9) {
-        result.textContent = "Normal";
-    } else if (bmi < 29.9) {
-        result.textContent = "Kelebihan Berat Badan (Overweight)";
-    } else if (bmi > 29.9) {
-        result.textContent = "Obesitas (Obesity)";
-    } else if (w === "" || h === "") {
-        alert("Tolong masukan tinggi dan berat badan anda \n(Please enter your weight and height)");
-    } 
+        if (bmi < 18.5) {
+            result.textContent = "Kekurangan Berat Badan (Underweight)";
+        } else if (bmi < 24.9) {
+            result.textContent = "Normal";
+        } else if (bmi < 29.9) {
+            result.textContent = "Kelebihan Berat Badan (Overweight)";
+        } else {
+            result.textContent = "Obesitas (Obesity)";
+        }
+    }
 }
 
 // Reset
 function reset() {
-    w = "";
-    h = "";
+    weight.value = "";
+    height.value = "";
     result.textContent = "";
-}
-
-// memilih kelamin  || picks gender
-function rotate() {
- if (gender.textContent === "Pria") {
-    gender.textContent = "Wanita";
- } else {
-    gender.textContent = "Pria";
- }
 }
